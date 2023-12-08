939faa8a7562c6c3a06b31758162ff52bd35631a
talrejalavina <talreja.la@northeastern.edu>
2023-12-07 22:44:06 -0500

    Added Employee Model Class

---
+++ src/main/java/com/example/employeecrudapplication/model/Employee.java
@@ -0,0 +1,81 @@
+package com.example.employeecrudapplication.model;
+
+public class Employee {
+
+  private long id;
+  private String firstName;
+  private String lastName;
+  private String email;
+  private String phone;
+  private String title;
+  private String department;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDepartment() {
+    return department;
+  }
+
+  public void setDepartment(String department) {
+    this.department = department;
+  }
+
+  @Override
+  public String toString() {
+    return "Employee{" +
+            "id=" + id +
+            ", firstName='" + firstName + '\'' +
+            ", lastName='" + lastName + '\'' +
+            ", email='" + email + '\'' +
+            ", phone='" + phone + '\'' +
+            ", title='" + title + '\'' +
+            ", department='" + department + '\'' +
+            '}';
+  }
+}
