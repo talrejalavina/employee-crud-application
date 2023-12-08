8a539d7ed94157dd71ce24016567649d2ff93070
talrejalavina <talreja.la@northeastern.edu>
2023-12-08 00:30:25 -0500

    Added Update Employee API

--- src/main/java/com/example/employeecrudapplication/controller/EmployeeController.java
+++ src/main/java/com/example/employeecrudapplication/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,8 +37,21 @@ public class EmployeeController {
   }
 
   @GetMapping("/{id}")
-  public Employee getEmployeeById(@PathVariable Long id) {
+  public Employee getEmployeeById(@PathVariable long id) {
     return employeeRepository.findById(id).orElseGet(() -> null);
+  }
+
+  @PutMapping(value = "update/{id}")
+  public String updateUser(@PathVariable long id, @RequestBody Employee employee) {
+    Employee updatedEmployee = employeeRepository.findById(id).get();
+    updatedEmployee.setFirstName(employee.getFirstName());
+    updatedEmployee.setLastName(employee.getLastName());
+    updatedEmployee.setEmail(employee.getEmail());
+    updatedEmployee.setPhone(employee.getPhone());
+    updatedEmployee.setTitle(employee.getTitle());
+    updatedEmployee.setDepartment(employee.getDepartment());
+    employeeRepository.save(updatedEmployee);
+    return "Updated Employee Details.";
   }
 
 }
