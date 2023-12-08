7057cda345670c9bdf4a95d48e2e94c6a25807a5
talrejalavina <talreja.la@northeastern.edu>
2023-12-08 00:03:27 -0500

    Added Read Employee API

--- src/main/java/com/example/employeecrudapplication/controller/EmployeeController.java
+++ src/main/java/com/example/employeecrudapplication/controller/EmployeeController.java
@@ -5,9 +5,13 @@ import com.example.employeecrudapplication.repository.EmployeeRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.List;
 
 @RestController
 public class EmployeeController {
@@ -24,6 +28,16 @@ public class EmployeeController {
   public String createEmployee(@RequestBody Employee employee) {
     employeeRepository.save(employee);
     return "New Employee Added.";
+  }
+
+  @GetMapping(value = "/employees")
+  public List<Employee> getAllEmployees() {
+    return employeeRepository.findAll();
+  }
+
+  @GetMapping("/{id}")
+  public Employee getEmployeeById(@PathVariable Long id) {
+    return employeeRepository.findById(id).orElseGet(() -> null);
   }
 
 }
